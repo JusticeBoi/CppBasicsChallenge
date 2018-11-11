5834711f01ee98dd54b12977d348a5845526225d
Oztoprak <oguz.oztoprak@tum.de>
2018-11-11 20:12:13 +0100

    clean up, trying openmp, the int is also of size digits now

--- src/lib.cpp
+++ src/lib.cpp
@@ -17,38 +17,6 @@ int fast_char_to_int(const char *p) {
             return x;
 }
 
-//std::vector<std::string> produceNumberOfEntries(size_t number_of_entries)
-//		{
-//	std::vector<std::string> vec_of_string;
-//		for (size_t i = 0 ; i< number_of_entries ; ++i)
-//		{
-//			vec_of_string.emplace_back(produceEntry(6));
-//		}
-//		return vec_of_string;
-//		}
-//void writeEntries_2(const std::vector<std::string>& vec_strings)
-//{
-//	std::ofstream outf("outfile2.bin",std::ios::binary);
-//	auto size = vec_strings[0].size();
-//
-//	for(auto& strng : vec_strings)
-//	{
-//		outf.write(strng.c_str(),sizeof(char)*size+1);
-//	}
-//	outf.close();
-//
-//}
-//void writeEntries_1(size_t number_of_entries,size_t size_of_entry)
-//{
-//	std::ofstream outf("outfile1.bin",std::ios::binary);
-//	for(size_t i = 0 ; i <number_of_entries ; ++i)
-//	{
-//		outf.write(produceEntry(size_of_entry).c_str(),sizeof(char)*size_of_entry +1);
-////		outf << std::ios::binary<<produceEntry(size_of_entry) <<std::endl;
-//	}
-//	outf.close();
-//}
-//
 void produceAndWriteEntriesInBinaryWithSize(int size,int number_of_entries)
 {
 
@@ -57,22 +25,22 @@ void produceAndWriteEntriesInBinaryWithSize(int size,int number_of_entries)
     int entry_int;
 
 	std::mt19937 rng(std::random_device{}());
-
-	std::uniform_int_distribution<std::mt19937::result_type> dist(1,std::numeric_limits<int>::max());
+	//std::uniform_int_distribution<std::mt19937::result_type> dist(1,std::numeric_limits<int>::max());
+	std::uniform_int_distribution<std::mt19937::result_type> dist(std::pow(10,size),std::pow(10,size+1)-1);
 
 	std::ofstream outf("outfile_final.bin",std::ios::binary );
 
 	//size_t size_str = sizeof(char) * size;
-	size_t size_alpha_str = sizeof(alpha) - 1 ;
+	constexpr size_t size_alpha_str = sizeof(alpha) - 1 ;
     std::cout << "Generating random entries and writing them in binary ...\n";
     srand(time(NULL));
+    //#pragma omp parallel for  
 	for(int ent = 0 ; ent < number_of_entries ; ++ent)
 	{
 	    std::string entry_str;
         entry_int = dist(rng);
         outf.write(reinterpret_cast<const char*>(&entry_int),sizeof(int));
 		//outf.write(&tab,sizeof(char));
-
 		for (int i = 0; i < size; ++i) {
 			entry_str += alpha[rand() % (size_alpha_str)];
 		}
@@ -129,27 +97,6 @@ std::vector<std::pair<int,std::string>> readBinFile_vector(int size, int number_
 		}
 
 
-//std::multimap<int,std::string> readBinFile_multi_map(int number_of_entries){
-//	
-//    std::ifstream inf("outfile_final.bin",std::ios::binary);
-//	std::string string_part = std::string();
-//	std::string int_part = std::string();
-//    std::multimap<int,std::string> mmap;
-//
-//    if (inf.is_open()){
-//	    for (int i = 0 ; i < number_of_entries ;++i)
-//	    {
-//	    	inf >> int_part;
-//	    	inf >> string_part;
-//            //vec_of_pairs.emplace_back(std::stoi(int_part),string_part);
-//            //-----BELOW IS POSSIBLY FASTER BUT LESS SAFE VERSION OF stoi, C.style.
-//            mmap.emplace(fast_char_to_int(int_part.c_str()),string_part);
-//	    }
-//        }
-//	inf.close();
-//	return mmap;
-//
-//}
 void writeSortedEntriesASCII(const std::vector<std::pair<int,std::string>> &  vec_to_be_written)
 {
 	//std::ofstream outf("final_result_ascii.txt",std::ios::out);
@@ -169,49 +116,3 @@ void writeSortedEntriesASCII(const std::vector<std::pair<int,std::string>> &  ve
         });
     std::fclose(fp);
 }
-//void writeSortedEntriesASCII_mmap(const std::multimap<int,std::string>&  mmap)
-//{
-//	std::ofstream outf("final_result_ascii.txt");
-//
-//
-//	char tab = '\t';
-//	char space = ' ';
-//	char endl ='\n';
-//	for(auto& int_string_pair : mmap)
-//	{
-//		outf << int_string_pair.first;
-//		outf.write(&tab,sizeof(char));
-//
-//		for( auto& a_char : int_string_pair.second)
-//		{
-//			outf<<(int)a_char;
-//			outf.write(&space,sizeof(char));
-//		}
-////		outf<<end_line;
-//		outf.write(&endl,sizeof(char));
-//	}
-//	outf.close();
-//}
-
-
-// previous trials with maps
-//std::map<int,std::string> readBinFile_map(int number_of_entries)
-//		{
-//			std::ifstream inf("outfile_final.bin",std::ios::binary);
-//			std::string string_part = std::string();
-//			std::string int_part = std::string();
-//			std::map<int,std::string> readFile;
-//
-//			for (int i = 0 ; i < number_of_entries ;++i)
-//			{
-//			inf >> int_part;
-//			inf >> string_part;
-//			readFile.insert(std::make_pair(std::stoi(int_part),string_part));
-//			}
-//			inf.close();
-//			return readFile;
-//
-//		}
-//
-//
-
--- src/lib.hpp
+++ src/lib.hpp
@@ -10,21 +10,16 @@
 #include <parallel/algorithm>
 #include <array>
 #include <cstdio>
+#include "omp.h"
 
 static const char alpha[] =
 	        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	        "abcdefghijklmnopqrstuvwxyz";
-//static const char num[] = "0123456789";
+	        "abcdefghijklmnopqrstuvwxyz'/?.!%$&;<>=:";
 
-std::string  produceEntry(size_t);
-void writeEntries_1(size_t number_of_entries,size_t size_of_entry);
 
-std::vector<std::string> produceNumberOfEntries(size_t number_of_entries);
-void writeEntries_2(const std::vector<std::string>& vec_strings);
 
 void produceAndWriteEntriesInBinaryWithSize(int size,int number_of_entries);
 
-std::map<int,std::string> readBinFile_map(int number_of_entries);
 std::vector<std::pair<int,std::string>> readBinFile_vector(int size, int number_of_entries);
 
 
@@ -34,5 +29,3 @@ void sortVecOfPair(std::vector<std::pair<int,std::string>>&);
 void writeSortedEntriesASCII(const std::vector<std::pair<int,std::string>> &  to_be_written);
 
 int fast_char_to_int(const char *p); 
-std::multimap<int,std::string> readBinFile_multi_map(int number_of_entries);
-//void writeSortedEntriesASCII_mmap(const std::multimap<int,std::string>&  mmap);
